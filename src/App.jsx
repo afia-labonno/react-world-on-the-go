89b4f8a832c919e0af0c93a876e01d9ac8785a39
afia-labonno <afia.cse01@gmail.com>
2026-03-19 09:50:17 +0600

    Country added with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
+import { Suspense } from 'react'
 import './App.css'
-import Countries from './components/countries/Countries'
+import Countries from './components/Countries/Countries'
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then(response => response.json())
+
 
 function App() {
 
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<p>Counstries Loading...</p>}>
+          <Countries countriesPromise = {countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Country.css' ;
+
+const Country = ({country}) => {
+    console.log(country.area.area);
+
+    // event handler
+    const handleVisited = () =>{
+        alert("Button Clicled")
+    }
+
+    return (
+        <div className='country'>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
+            <h3>Name : {country.name.common}</h3>
+            <p>Population : {country.population.population}</p>
+            <p>Capital : {country.capital.capital}</p>
+
+            <p>Area : {country.area.area} {
+                country.area.area > 300000 ? "Big Countrty" : "Small Country"}
+            </p>
+
+            <button onClick={handleVisited}>Not Visited</button>
+
+        </div>
+    );
+};
+
+export default Country;
+
+
+/**
+ * 1. inline css (style object)
+ */
